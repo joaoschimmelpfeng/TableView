6ac4f8fe7ff48e1e961e96319b65f4c19c627d59
Fernando Baptista <fernandohsb@gmail.com>
2015-02-25 15:10:51 -0300

    b

---
+++ tableProject/tableProject/AppTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  AppTableViewCell.h
+//  tableProject
+//
+//  Created by Fernando on 2/25/15.
+//  Copyright (c) 2015 João Vitor dos Santos Schimmelpfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppTableViewCell : UITableViewCell
+jjjjjjj;
+@end
---
+++ tableProject/tableProject/AppTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  AppTableViewCell.m
+//  tableProject
+//
+//  Created by Fernando on 2/25/15.
+//  Copyright (c) 2015 João Vitor dos Santos Schimmelpfeng. All rights reserved.
+//
+//aquiiiii
+#import "AppTableViewCell.h"
+
+@implementation AppTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
