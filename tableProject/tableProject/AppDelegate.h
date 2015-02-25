0eba8130547b8a9738a7efe547ddb8f9e3585f27
Joao <joaoschimmelpfeng@gmail.com>
2015-02-25 14:47:43 -0300

    Push inicial
    push.

---
+++ tableProject/tableProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tableProject
+//
+//  Created by João Vitor dos Santos Schimmelpfeng on 25/02/15.
+//  Copyright (c) 2015 João Vitor dos Santos Schimmelpfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
